b957e930bf7015d2c5a3c370550da77d679e875d
kronenthaler <kronenthaler@gmail.com>
2015-07-12 17:54:01 +0200

    removed py3 version and added pure-python support
    Signed-off-by: kronenthaler <kronenthaler@gmail.com>

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(name='mod_pbxproj',
-      author='Ignacio Calderon',
-      description='XCode Project Generator for Python',
-      url="http://github.com/kronenthaler/mod-pbxproj",
-      version='1.0',
-      license='BSD License',
-      packages=find_packages(exclude=['tests']))
+    author='Ignacio Calderon',
+    description='XCode Project Generator for Python',
+    url="http://github.com/kronenthaler/mod-pbxproj",
+    version='1.0',
+    license='BSD License',
+    install_requires = ['openstep_parser'],
+    packages=find_packages(exclude=['tests']))
