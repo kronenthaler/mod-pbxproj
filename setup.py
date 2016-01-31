82dc7f56d094043aa6edd6e11f12c072cf52c481
kronenthaler <kronenthaler@gmail.com>
2016-01-31 13:49:26 +0100

    chore: travis coverage

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
+
+# Inspired by the example at https://pytest.org/latest/goodpractises.html
+class NoseTestCommand(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Run nose ensuring that argv simulates running nosetests directly
+        import nose
+        nose.run_exit(argv=['nosetests','-w','tests'])
 
 setup(name='mod_pbxproj',
     author='Ignacio Calderon',
@@ -8,4 +21,7 @@ setup(name='mod_pbxproj',
     version='1.3',
     license='BSD License',
     install_requires = ['openstep_parser'],
-    packages=find_packages(exclude=['tests']))
+    packages=find_packages(exclude=['tests']),
+    setup_requires=['nose', 'coverage'],
+    cmdclass={'test': NoseTestCommand}
+)
