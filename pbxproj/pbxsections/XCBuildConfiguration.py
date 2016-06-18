c6e2171925d93eabc52dc6fbc900e408539dc22c
kronenthaler <kronenthaler@gmail.com>
2016-06-18 23:17:47 +0200

    feat: remove other_cflags and other_ldflags from the project

--- pbxproj/pbxsections/XCBuildConfiguration.py
+++ pbxproj/pbxsections/XCBuildConfiguration.py
@@ -2,13 +2,22 @@ from pbxproj import PBXGenericObject
 
 
 class XCBuildConfiguration(PBXGenericObject):
+    OTHER_CFLAGS = u'OTHER_CFLAGS'
+    OTHER_LDFLAGS = u'OTHER_LDFLAGS'
+
     def add_other_cflags(self, flags):
-        self._add_flag(u'OTHER_CFLAGS', flags)
+        self._add_flags(XCBuildConfiguration.OTHER_CFLAGS, flags)
+
+    def remove_other_cflags(self, flags):
+        self._remove_flags(XCBuildConfiguration.OTHER_CFLAGS, flags)
 
     def add_other_ldflags(self, flags):
-        self._add_flag(u'OTHER_LDFLAGS', flags)
+        self._add_flags(XCBuildConfiguration.OTHER_LDFLAGS, flags)
 
-    def _add_flag(self, flag_name, flags):
+    def remove_other_ldflags(self, flags):
+        self._remove_flags(XCBuildConfiguration.OTHER_LDFLAGS, flags)
+
+    def _add_flags(self, flag_name, flags):
         if u'buildSettings' not in self:
             self[u'buildSettings'] = PBXGenericObject()
 
@@ -24,3 +33,22 @@ class XCBuildConfiguration(PBXGenericObject):
             flags = [flags]
 
         self.buildSettings[flag_name] += flags
+
+    def _remove_flags(self, flag_name, flags):
+        if u'buildSettings' not in self or self.buildSettings[flag_name] is None:
+            # nothing to remove
+            return
+
+        current_flags = self.buildSettings[flag_name]
+        if not isinstance(current_flags, list):
+            current_flags = [current_flags]
+
+        if not isinstance(flags, list):
+            flags = [flags]
+
+        self.buildSettings[flag_name] = [x for x in current_flags if x not in flags]
+
+        if self.buildSettings[flag_name].__len__() == 1:
+            self.buildSettings[flag_name] = self.buildSettings[flag_name][0]
+        elif self.buildSettings[flag_name].__len__() == 0:
+            del self.buildSettings[flag_name]
--- tests/pbxsections/TestXCBuildConfiguration.py
+++ tests/pbxsections/TestXCBuildConfiguration.py
@@ -5,7 +5,7 @@ from pbxproj.pbxsections.XCBuildConfiguration import *
 class XCBuildConfigurationTest(unittest.TestCase):
     def testAddFlagOnNewObject(self):
         obj = XCBuildConfiguration()
-        obj._add_flag('flag', '-flag')
+        obj._add_flags('flag', '-flag')
 
         self.assertIsNotNone(obj.buildSettings)
         self.assertIsNotNone(obj.buildSettings.flag)
@@ -13,7 +13,7 @@ class XCBuildConfigurationTest(unittest.TestCase):
 
     def testAddFlagsOnNewObject(self):
         obj = XCBuildConfiguration()
-        obj._add_flag('flag', ['-flag','-another-flag'])
+        obj._add_flags('flag', ['-flag', '-another-flag'])
 
         self.assertIsNotNone(obj.buildSettings)
         self.assertIsNotNone(obj.buildSettings.flag)
@@ -23,29 +23,57 @@ class XCBuildConfigurationTest(unittest.TestCase):
         obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'flag': '-flag'}}
         dobj = XCBuildConfiguration().parse(obj)
 
-        dobj._add_flag('flag', '-another-flag')
+        dobj._add_flags('flag', '-another-flag')
         self.assertListEqual(dobj.buildSettings.flag, ['-flag', '-another-flag'])
 
     def testAddFlagOnMultipleFlags(self):
         obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'flag': ['-flag', '-b-flag']}}
         dobj = XCBuildConfiguration().parse(obj)
 
-        dobj._add_flag('flag', '-another-flag')
+        dobj._add_flags('flag', '-another-flag')
         self.assertListEqual(dobj.buildSettings.flag, ['-flag', '-b-flag', '-another-flag'])
 
     def testAddFlagsOnSingleFlag(self):
         obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'flag': '-flag'}}
         dobj = XCBuildConfiguration().parse(obj)
 
-        dobj._add_flag('flag', ['-another-flag'])
+        dobj._add_flags('flag', ['-another-flag'])
         self.assertListEqual(dobj.buildSettings.flag, ['-flag', '-another-flag'])
 
     def testAddFlagsOnMultipleFlags(self):
         obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'flag': ['-flag', '-b-flag']}}
         dobj = XCBuildConfiguration().parse(obj)
 
-        dobj._add_flag('flag', ['-another-flag'])
+        dobj._add_flags('flag', ['-another-flag'])
         self.assertListEqual(dobj.buildSettings.flag, ['-flag', '-b-flag', '-another-flag'])
+
+    def testRemoveFlagOnEmpty(self):
+        obj = {'isa': 'XCBuildConfiguration'}
+        dobj = XCBuildConfiguration().parse(obj)
+
+        dobj._remove_flags('flag', '-flag')
+        self.assertIsNone(dobj['buildSettings'])
+
+    def testRemoveFlagNonExistent(self):
+        obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'flag1': '-flag'}}
+        dobj = XCBuildConfiguration().parse(obj)
+
+        dobj._remove_flags('flag', '-flag')
+        self.assertIsNone(dobj.buildSettings['flag'])
+
+    def testRemoveFlagOnSingleValue(self):
+        obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'flag': '-flag'}}
+        dobj = XCBuildConfiguration().parse(obj)
+
+        dobj._remove_flags('flag','-flag')
+        self.assertIsNone(dobj.buildSettings['flag'])
+
+    def testRemoveFlagOnMultipleValue(self):
+        obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'flag': ['-flag', '-b-flag']}}
+        dobj = XCBuildConfiguration().parse(obj)
+
+        dobj._remove_flags('flag', '-flag')
+        self.assertEqual(dobj.buildSettings.flag, '-b-flag')
 
     def testAddOtherCFlags(self):
         obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'OTHER_CFLAGS': ['-flag', '-b-flag']}}
@@ -60,3 +88,18 @@ class XCBuildConfigurationTest(unittest.TestCase):
 
         dobj.add_other_ldflags(['-another-flag'])
         self.assertListEqual(dobj.buildSettings.OTHER_LDFLAGS, ['-flag', '-b-flag', '-another-flag'])
+
+    def testRemoveOtherCFlags(self):
+        obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'OTHER_CFLAGS': ['-flag', '-b-flag', '-another-flag']}}
+        dobj = XCBuildConfiguration().parse(obj)
+
+        dobj.remove_other_cflags(['-flag'])
+        self.assertListEqual(dobj.buildSettings.OTHER_CFLAGS, ['-b-flag', '-another-flag'])
+
+    def testRemoveOtherLDFlags(self):
+        obj = {'isa': 'XCBuildConfiguration', 'buildSettings': {'OTHER_LDFLAGS': ['-flag', '-b-flag', '-another-flag']}}
+        dobj = XCBuildConfiguration().parse(obj)
+
+        dobj.remove_other_ldflags(['-flag'])
+        self.assertListEqual(dobj.buildSettings.OTHER_LDFLAGS, ['-b-flag', '-another-flag'])
+
