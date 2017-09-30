32b6b097bbbb727fdda45a2e83092389df05c8cc
kronenthaler <kronenthaler@gmail.com>
2017-09-30 13:39:45 +0200

    chore: pump version 2.1.2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name='pbxproj',
         ]
       },
       url="http://github.com/kronenthaler/mod-pbxproj",
-      version='2.1.1',
+      version='2.1.2',
       license='MIT License',
       install_requires=['openstep_parser', 'docopt'],
       packages=find_packages(exclude=['tests']),
