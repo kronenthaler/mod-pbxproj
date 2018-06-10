fa82883576a659d9cd9d830919e744299ac14ac7
kronenthaler <kronenthaler@gmail.com>
2018-06-10 14:32:57 +0200

    feat: improve show command to show target types, build phase types and filter other build phases.

--- pbxproj/pbxcli/pbxproj_show.py
+++ pbxproj/pbxcli/pbxproj_show.py
@@ -1,7 +1,11 @@
 """
 usage:
     pbxproj show [options] <project>
-    pbxproj show [options] (--target <target>...) <project> [(-s | --source-files) | (-H | --header-files) | (-r | --resource-files) | (-f | --framework-files)]
+    pbxproj show [options] (--target <target>...) <project> [(-s | --source-files) |
+                                                             (-H | --header-files) |
+                                                             (-r | --resource-files) |
+                                                             (-f | --framework-files) |
+                                                             (--build-phase-files <build_phase_type>)]
 
 positional arguments:
     <project>                      Project path to the .xcodeproj folder.
@@ -17,9 +21,11 @@ target options:
     -f, --framework-files           Show the library files attached to the target
     -H, --header-files              Show the header files attached to the target
     -c, --configurations            Show the configurations attached to the target
+    -B, --build-phase-files <type>  Show the files associated to the build phase of the given type.
 """
 
 from pbxproj.pbxcli import *
+
 
 def execute(project, args):
     # make a decision of what function to call based on the -D flag
@@ -33,17 +39,19 @@ def _summary(project, args):
     info = ''
     for target in project.objects.get_targets():
         info += "{name}:\n" \
+                "\tTarget type: {type}\n" \
                 "\tProduct name: {productName}\n" \
                 "\tConfigurations: {configs}\n" \
             .format(name=target.name,
                     productName=target.productName,
+                    type=target.isa,
                     configs=', '.join([c.name for c in project.objects.get_configurations_on_targets(target.name)]),
                     )
 
         for build_phase_id in target.buildPhases:
             build_phase = project.objects[build_phase_id]
-            info += "\t{name} count: {count}\n"\
-                .format(name=build_phase._get_comment(), count=build_phase.files.__len__())
+            info += "\t{name} ({type}) file count: {count}\n"\
+                .format(name=build_phase._get_comment(), type=build_phase.isa, count=build_phase.files.__len__())
 
         info += "\n"
     return info
@@ -59,6 +67,8 @@ def _target_info(project, target_name, args):
         build_phases += [u'PBXResourcesBuildPhase']
     elif args[u'--framework-files']:
         build_phases += [u'PBXFrameworksBuildPhase']
+    elif args[u'--build-phase-files']:
+        build_phases += [args[u'--build-phase-files']]
 
     info = ''
     for target in project.objects.get_targets(target_name):
